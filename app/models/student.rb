8321f5934ef658d276085a567b563cd5c0e16b90
curtisgreene <curtis.j.w.greene@gmail.com>
2017-04-10 11:06:44 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_message
+    if !self.active
+      "This student is currently inactive."
+    elsif self.active
+      "This student is currently active."
+    end
+  end
+
 end
